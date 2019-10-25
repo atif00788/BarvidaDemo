f4009568c95fad14910be295c60997f9f7232964
atif00788 <atif00788@gmail.com>
2019-10-25 22:48:53 +0200

    Updates

---
+++ precache-manifest.314210d55d81b3b0633dfe12bfdf610f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e73a9f478eb1180a3de818ff2b233d58",
+    "url": "/atif00788/BarvidaDemo/index.html"
+  },
+  {
+    "revision": "3ef4793578d867d499c9",
+    "url": "/atif00788/BarvidaDemo/static/css/main.31e3e46b.chunk.css"
+  },
+  {
+    "revision": "6dbf19a531cd39d22fb5",
+    "url": "/atif00788/BarvidaDemo/static/js/2.d2b0494b.chunk.js"
+  },
+  {
+    "revision": "3ef4793578d867d499c9",
+    "url": "/atif00788/BarvidaDemo/static/js/main.b64c5818.chunk.js"
+  },
+  {
+    "revision": "a82393c1f0573df601ad",
+    "url": "/atif00788/BarvidaDemo/static/js/runtime-main.311b38f9.js"
+  }
+]);
